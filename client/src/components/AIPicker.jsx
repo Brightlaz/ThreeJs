1b7a1d8dd254a3de0e4b2e71f6496d298cf002cb
Bright Lazarus <lazbright1@gmail.com>
2023-04-09 18:14:13 +0100

    Encountered an error in the server side

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -2,7 +2,42 @@ import React from "react";
 import CustomButton from "./CustomButton";
 
 const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
-  return <div>AIPicker</div>;
+  return (
+    <div className="aipicker-container">
+      <textarea
+        placeholder="Ask AI..."
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        className="aipicker-textarea"
+      />
+      <div>
+        {generatingImg ? (
+          <CustomButton
+            type="outline"
+            title="Asking AI..."
+            customStyles="text-xs"
+          />
+        ) : (
+          <>
+            <CustomButton
+              type="outline"
+              title="AI logo"
+              handleClick={() => handleSubmit("logo")}
+              customStyles="text-xs"
+            />
+
+            <CustomButton
+              type="filled"
+              title="AI Full"
+              handleClick={() => handleSubmit("full")}
+              customStyles="text-xs"
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AIPicker;
