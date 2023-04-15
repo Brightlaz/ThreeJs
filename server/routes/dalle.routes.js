36689020ed56802876bdac5f72bb0c0c3ae1dd97
Bright Lazarus <lazbright1@gmail.com>
2023-04-15 18:34:35 +0100

    Debug complete

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,7 +1,6 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import { Configuration, OpenAIApi } from "openai";
-import Joi from "joi";
 
 dotenv.config();
 
@@ -13,42 +12,27 @@ const config = new Configuration({
 
 const openai = new OpenAIApi(config);
 
-if (!process.env.OPENAI_API_KEY) {
-  throw new Error("OPENAI_API_KEY environment variable is not set");
-}
-
-const promptSchema = Joi.object({
-  prompt: Joi.string().min(1).max(1000).required(),
-});
-
 router.route("/").get((req, res) => {
   res.status(200).json({ message: "Hello from DALL.E ROUTES" });
 });
 
 router.route("/").post(async (req, res) => {
   try {
-    const { error, value } = promptSchema.validate(req.body);
-    if (error) {
-      throw new Error(error.details[0].message);
-    }
+    const { prompt } = req.body;
 
     const response = await openai.createImage({
-      prompt: value.prompt,
+      prompt,
       n: 1,
       size: "1024x1024",
       response_format: "b64_json",
     });
-
-    if (response.data.error) {
-      throw new Error(response.data.error.message);
-    }
 
     const image = response.data.data[0].b64_json;
 
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: "Something went wrong" });
   }
 });
 
