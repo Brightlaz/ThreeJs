0ae64e89350ceb0552e5c9c2f7731610bdc2e8bf
Bright Lazarus <lazbright1@gmail.com>
2023-05-21 21:52:09 +0100

    Removed the color picker function

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { SketchPicker } from "react-color";
 import { FaEyeDropper } from "react-icons/fa";
 import { useSnapshot } from "valtio";
-import { extractColorFromScreen } from "react-color-extractor";
 
 import state from "../store";
 
@@ -10,23 +9,6 @@ const ColorPicker = () => {
   const snap = useSnapshot(state);
   const [isEyedropperActive, setIsEyedropperActive] = useState(false);
   const [eyedropperColor, setEyedropperColor] = useState(snap.color);
-
-  useEffect(() => {
-    if (isEyedropperActive) {
-      const handleMouseMove = (e) => {
-        extractColorFromScreen(e.clientX, e.clientY).then((color) => {
-          setEyedropperColor(color);
-          state.color = color;
-        });
-      };
-
-      window.addEventListener("mousemove", handleMouseMove);
-
-      return () => {
-        window.removeEventListener("mousemove", handleMouseMove);
-      };
-    }
-  }, [isEyedropperActive]);
 
   const handleEyedropperClick = () => {
     setIsEyedropperActive(!isEyedropperActive);
